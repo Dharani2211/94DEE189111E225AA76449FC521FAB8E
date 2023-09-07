c10103f20b45d4031bafef24f72446f52c289d82
Dharani2211 <144316642+Dharani2211@users.noreply.github.com>
2023-09-07 12:20:32 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,7 @@
+1.1
+def fact_rec(n):
+  if n = = 0 or n = = 1:
+  return1
+else:
+return n* fact_rec(n-1)
+print ("the factorial or {}is{}" formal(number.rec))
---
+++ main (2).py
@@ -0,0 +1,16 @@
+#leap year
+" " "
+year % 4 = = 0 &
+year % 100! =0/
+year % 400 = = 0
+' ' '
+def isleap year (year):
+if (year % 4 = = 0 and  year %100! =0) or (year % 400 = = 0 !)
+return true 
+else:
+return false 
+year = 2012
+if is leapyear (year):
+  print('{} is a leap year').'.(year)
+else:
+  print('{} is not a leap year.'. format (year))
